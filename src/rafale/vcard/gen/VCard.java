1d5e97486f7216e4764c24f23620db661e42ef19
Speedy321 <Christo-Chibougamo@hotmail.com>
2019-10-31 13:22:59 -0400

    vCard: reformatted a few thing, added the option to choose McGill/ETS

--- src/rafale/vcard/gen/VCard.java
+++ src/rafale/vcard/gen/VCard.java
@@ -11,14 +11,24 @@ package rafale.vcard.gen;
  */
 public class VCard {
     
+    private static int sOrgNB = 2;
+    public static enum SubOrg{
+        ETS,
+        MCGILL,
+    }
+    
     private String name = "";
     private String surname = "";
     private String phone = "";
     private String email = "";
     private String job = "";
     
-    private final String organism = "Rafale - ETS";
-    private final String emailEnd = "@ens.etsmtl.ca";
+    private SubOrg subOrg = SubOrg.ETS;
+    
+    private final String organism = "RAFALE - ÉTS";
+    private final String website = "www.rafale-ets.com";
+    public static final String emailEnd = "@ens.etsmtl.ca";
+    public static final String emailMcGillEnd = "@mail.mcgill.ca";
     
     private final String vCardBegin = "BEGIN:VCARD";
     private final String vCardVersion = "\nVERSION:3.0";
@@ -35,37 +45,59 @@ public class VCard {
         
     }
     
-    public VCard(String name, String surname, String phone, String emailStub, String job){
-        this.name = name;
-        this.surname = surname;
-        this.phone = phone;
-        this.email = emailStub + this.emailEnd;
-        this.job = job;
+    public VCard(String name, String surname, String phone, SubOrg subOrg, String emailStub, String job){
+        this.setName(name);
+        this.setSurname(surname);
+        this.setPhone(phone);
+        this.setSubOrg(subOrg);
+        this.setEmail(emailStub);
+        this.setJob(job);
     }
     
     String getName() { return this.name; }
     String getSurname() { return this.surname; }
     String getPhone() { return this.phone; }
     String getFormatedPhone() {
-        String forPhone = "(";
+        if(!this.phone.equals("")){
+            String forPhone = "+(1) ";
         
-        forPhone += this.phone.substring(0, 3);
-        forPhone += ") ";
-        forPhone += this.phone.substring(3, 6);
-        forPhone += "-";
-        forPhone += this.phone.substring(6, 10);
+            forPhone += this.phone.substring(0, 3);
+            forPhone += "-";
+            forPhone += this.phone.substring(3, 6);
+            forPhone += "-";
+            forPhone += this.phone.substring(6, 10);
         
-        return forPhone;
+            return forPhone;
+        } else
+            return "";
     }
     String getEmail() { return this.email; }
     String getJob() { return this.job; }
     String getOrganism() { return this.organism; }
+    String getWebsite() { return this.website; }
+    
+    public static String[] getSubOrgs() {
+        String list[] = {"", ""};
+        
+        list[0] = VCard.emailEnd;
+        list[1] = VCard.emailMcGillEnd;
+        
+        return list;
+    }
     
     void setName(String name) { this.name = name; }
     void setSurname(String surname) { this.surname = surname; }
     void setPhone(String phone) { this.phone = phone; }
-    void setEmail(String emailStub) { this.email = email + this.emailEnd; }
+    
+    void setEmail(String emailStub) { 
+        if(subOrg == SubOrg.ETS)
+            this.email = emailStub + this.emailEnd;
+        else if (subOrg == SubOrg.MCGILL)
+            this.email = emailStub + this.emailMcGillEnd;
+    }
+    
     void setJob(String job) { this.job = job; }
+    void setSubOrg(SubOrg subOrg){ this.subOrg = subOrg; }
     
     String getAsString(){
         String vCard = "";
